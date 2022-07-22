0c0c95c8c8b72297c748e289b560f91fee17cbbc
Chomolungma-Poem <jerryhuang0621@qq.com>
2022-07-22 09:44:31 +0800

    XMLCheckerLib/typing

--- document/xc/xc.py
+++ document/xc/xc.py
@@ -1,27 +1,27 @@
-from builtins import object
+from builtins import object, dict, str, bool, list
 
-ANY_TEXTS = { "" : "" }
-ANY_ELEMENTS = { "" : "" }
-ANY_ATTRIBUTES = { "" : "" }
+ANY_TEXTS : dict [ str, str ] = { "" : "" }
+ANY_ELEMENTS : dict= { "" : "" }
+ANY_ATTRIBUTES : dict = { "" : "" }
 
 class TextType ( object ):
-    def check ( self, string ):
+    def check ( self, string : str ) -> bool:
         return False
 
 class Empty ( TextType ):
-    def check ( self, string ):
+    def check ( self, string : str ) -> bool:
         return string == ""
 
 class String ( TextType ):
-    def check ( self, string ):
+    def check ( self, string : str ) -> bool:
         return True
 
 class Boolean ( TextType ):
-    def check ( self, string ):
+    def check ( self, string : str ) -> bool:
         return string == "true" or string == "false"
     
 class Number ( TextType ):
-    def check ( self, string ):
+    def check ( self, string : str ) -> bool:
         try:
             int ( string )
         except:
@@ -30,15 +30,15 @@ class Number ( TextType ):
             return True
 
 class ElementType ( object ):
-    def __init__ ( self, name ):
-        self.name = name
-        self.textType = String (  )
-        self.elementsIn = [  ]
-        self.attributesIn = [  ]
-        self.stdText = ""
+    def __init__ ( self, name : str ):
+        self.name : str = name
+        self.textType : TextType = String (  )
+        self.elementsIn : list [ ElementType ] = [  ]
+        self.attributesIn : list [ AttributeType ] = [  ]
+        self.stdText : str = ""
 
 class AttributeType ( object ):
     def __init__ ( self, name ):
-        self.name = name
-        self.textIn = [  ]
-        self.stdText = ""
+        self.name : str = name
+        self.textIn : TextType = String (  )
+        self.stdText : str = ""
