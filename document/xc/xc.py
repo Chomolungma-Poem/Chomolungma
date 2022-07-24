d4153926a54170eacbb519a6d9d56b2a825fee42
Chomolungma-Poem <jerryhuang0621@qq.com>
2022-07-24 09:30:45 +0800

    update cml.xc.py/xmlns

--- document/xc/xc.py
+++ document/xc/xc.py
@@ -1,33 +1,51 @@
 from builtins import object, dict, str, bool, list
+from re import  Match, compile, match, DOTALL
 
 ANY_TEXTS : dict [ str, str ] = { "" : "" }
 ANY_ELEMENTS : dict= { "" : "" }
 ANY_ATTRIBUTES : dict = { "" : "" }
 
 class TextType ( object ):
+    regex : str = None
     def check ( self, string : str ) -> bool:
-        return False
-
-class Empty ( TextType ):
-    def check ( self, string : str ) -> bool:
-        return string == ""
-
-class String ( TextType ):
-    def check ( self, string : str ) -> bool:
-        return True
-
-class Boolean ( TextType ):
-    def check ( self, string : str ) -> bool:
-        return string == "true" or string == "false"
-    
-class Number ( TextType ):
-    def check ( self, string : str ) -> bool:
-        try:
-            int ( string )
-        except:
+        matchans : Match | None = match ( compile ( self.regex ), string, DOTALL )
+        if matchans == None:
             return False
         else:
-            return True
+            return matchans.span [ 1 ] == len ( string )
+
+class Empty ( TextType ):
+    regex : str = ""
+
+class String ( TextType ):
+    regex : str = "."
+
+class Boolean ( TextType ):
+    regex : str = "true|false"
+
+class NumSymbol ( TextType ):
+    regex : str = "[0123456789]"
+
+class Number ( TextType ):
+    regex : str = "[123456789]{0}*(\.{0}*)?".format ( NumSymbol.regex )
+
+class LowerCase ( TextType ):
+    regex : str = "[abcdefghijklmnopqrstuvwxyz]"
+    
+class UpperCase ( TextType ):
+    regex : str = "[ABCDEFGHIJKLMNOPQRSTUVWXYZ]"
+
+class Letter ( TextType ):
+    regex : str = "{0}|{1}".format ( LowerCase.regex, UpperCase.regex )
+
+class Symbol ( TextType ):
+    regex : str = "{0}|{1}".format ( Letter.regex, NumSymbol.regex )
+
+class Word ( TextType ):
+    regex : str = "{0}*".format ( Letter.regex )
+    
+class TokenString ( TextType ):
+    regex : str = "{0}*".format ( Symbol.regex )
 
 class ElementType ( object ):
     def __init__ ( self, name : str ):
@@ -40,5 +58,5 @@ class ElementType ( object ):
 class AttributeType ( object ):
     def __init__ ( self, name ):
         self.name : str = name
-        self.textIn : TextType = String (  )
+        self.textType : TextType = String (  )
         self.stdText : str = ""
