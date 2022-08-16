1995d7e64cbce417540a3e8055702d22c3b9d8ab
Chomolungma-Poem <jerryhuang0621@qq.com>
2022-08-16 20:49:11 +0800

    compiler

---
+++ src/compiler/main.cpp
@@ -0,0 +1,18 @@
+// [main.cc] C++ Source
+
+// CHOMOLUNGM Programming Environment
+// Compiler Tool  (Dev 0.0.0.1)
+
+// Main file
+
+#include <string>
+
+#include "clipp.h"
+
+using namespace std;
+using namespace clipp;
+
+int main( int argc, char** argv ) {
+    
+    return 0;
+}
